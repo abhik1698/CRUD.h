65d0d15929c83f46e7d1ff811ffcdafc02783ee2
abhik1698 <abhik1698@gmail.com>
2019-11-22 13:25:47 +0530

    Update

--- index.php
+++ index.php
@@ -108,7 +108,7 @@
         }        
         }      
         ?>
-    <scri pt>
+    <script>
       function updateLogin(val){
         document.getElementById('id01').style.display='block';
         document.getElementById('uid').value= val;
